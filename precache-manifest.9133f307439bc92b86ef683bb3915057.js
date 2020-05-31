392bfddfd96268aa67df7cb3022dae06d2fe0bc9
Dario <dariokozicki@gmail.com>
2020-05-31 03:10:11 -0300

    Updates

---
+++ precache-manifest.9133f307439bc92b86ef683bb3915057.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6225a6442fd4d8622f6536f52455a643",
+    "url": "/client-free2play/index.html"
+  },
+  {
+    "revision": "8cdbc52d26cf56217a99",
+    "url": "/client-free2play/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "2f8d9a6d1b0ca8a46baa",
+    "url": "/client-free2play/static/css/main.29d5e47f.chunk.css"
+  },
+  {
+    "revision": "8cdbc52d26cf56217a99",
+    "url": "/client-free2play/static/js/2.dc2f8e58.chunk.js"
+  },
+  {
+    "revision": "6020e2578ce61e153ee5130f44d173fc",
+    "url": "/client-free2play/static/js/2.dc2f8e58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f8d9a6d1b0ca8a46baa",
+    "url": "/client-free2play/static/js/main.ffea4b97.chunk.js"
+  },
+  {
+    "revision": "7165569c88c2a7368dff",
+    "url": "/client-free2play/static/js/runtime-main.28e8df9f.js"
+  }
+]);
